cfbf930b5d8ff2930a242fbd40674ff7c7a16ff9
Mohammad <mohammad.rizwan@natwest.com>
2022-11-11 11:49:40 +0000

    Project in progress!

---
+++ src/main/java/TaxiCompany.java
@@ -0,0 +1,28 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.ArrayList;
+
+
+//in progress
+
+public class TaxiCompany {
+
+    private static final Logger LOGGER = LogManager.getLogger(TaxiCompany.class.getName());
+
+    private ArrayList<Taxi> taxis; public boolean hasAvailableTaxis() {
+        boolean hasTaxi = false;
+        for(Taxi taxi : taxis) {
+            if(taxi.isBooked()) {
+                LOGGER.info("Available taxi found " + taxi); }
+        }
+        return hasTaxi; }
+    public Taxi getNextTaxi() {
+        for(Taxi taxi : taxis) {
+            if(taxi.isBooked()) {
+                LOGGER.info("Available taxi found " + taxi); return taxi;
+            } }
+        return null; }
+    public void addTaxi(Taxi taxi) { taxis.add(new Taxi());
+    }
+}
+
